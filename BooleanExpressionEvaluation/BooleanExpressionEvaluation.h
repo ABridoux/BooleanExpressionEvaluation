d8d949c099d027a9e32069e82fc36afd45422d74
Alexis Bridoux <alexdroibux@gmail.com>
2019-11-27 08:39:22 +0100

    Initial Commit

---
+++ BooleanExpressionEvaluation/BooleanExpressionEvaluation.h
@@ -0,0 +1,19 @@
+//
+//  BooleanExpressionEvaluation.h
+//  BooleanExpressionEvaluation
+//
+//  Created by Alexis Bridoux on 27/11/2019.
+//  Copyright © 2019 Alexis Bridoux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BooleanExpressionEvaluation.
+FOUNDATION_EXPORT double BooleanExpressionEvaluationVersionNumber;
+
+//! Project version string for BooleanExpressionEvaluation.
+FOUNDATION_EXPORT const unsigned char BooleanExpressionEvaluationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BooleanExpressionEvaluation/PublicHeader.h>
+
+
