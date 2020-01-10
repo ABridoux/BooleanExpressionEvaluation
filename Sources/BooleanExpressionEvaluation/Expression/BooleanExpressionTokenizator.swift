8e502467eead395353c810d1aaf07933802c5846
Alexis Bridoux <alexdroibux@gmail.com>
2020-01-10 12:08:39 +0100

    Removed Podfile and Workspace

---
+++ Sources/BooleanExpressionEvaluation/Expression/BooleanExpressionTokenizator.swift
@@ -0,0 +1,249 @@
+//
+//  GNU GPLv3
+//
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see https://www.gnu.org/licenses
+    for more information.
+*/
+
+import Foundation
+
+/**
+ Responsible to transform an `Expression` with contains comparison as `variable > 2` to a boolean expression which
+ contains only `false` and `true`, the logic operators and brackets
+ */
+struct BooleanExpressionTokenizator {
+
+    // MARK: - Properties
+
+    var expression: Expression
+    var currentToken = ExpressionElement.logicOperator(.or)
+
+    /// Stores the variables which are present in the expression
+    var variables: [String: String]
+
+    // MARK: - Init
+
+    /**
+     - Parameters:
+        - expression: The expression to tokenize
+        - variables: Variables which are present in the expression
+     */
+    init(expression: Expression, variables: [String: String]) {
+        self.expression = expression
+        self.variables = variables
+    }
+
+    // MARK: - Functions
+
+    // MARK: Token validation
+
+    /**
+    Consume the next token in the expression
+     - returns: A token if one was found, or `nil` if the end of the expression is reached.
+     - note: Throws an error if a token chaining doesn't respect the grammar rules. The possible returned token are the following:
+        - LogicOperator
+        - Bracket
+        - Operand.Boolean
+    */
+    mutating func nextToken() throws -> ExpressionElement? {
+        var nextToken: ExpressionElement?
+
+        switch currentToken {
+        case .operand(.boolean):
+            nextToken = try currentTokenBooleanValidate()
+        case .logicOperator, .bracket(.opening):
+            nextToken = try currentTokenLogicOperatorOrOpeningBracketNextToken()
+        case .bracket(.closing):
+            nextToken = try currentTokenClosingBracketNextToken()
+        default:
+            break
+        }
+        currentToken = nextToken ?? currentToken
+        return nextToken
+    }
+
+    mutating func currentTokenBooleanValidate() throws -> ExpressionElement? {
+        guard let nextToken = expression.popFirst() else {
+            return nil
+        }
+        switch nextToken {
+        case .logicOperator, .bracket(.closing):
+            return nextToken
+        default:
+            throw ExpressionError.invalidExpression("Chaining a boolean with \(nextToken.description) is invalid")
+        }
+    }
+
+    /// Validate a next token when the current one is a logic operator or an opening bracket and returns it
+    mutating func currentTokenLogicOperatorOrOpeningBracketNextToken() throws -> ExpressionElement? {
+        guard let nextElement = expression.popFirst() else {
+            return nil
+        }
+
+        // validated if the next element is an opening bracket
+        if case let ExpressionElement.bracket(bracket) = nextElement, bracket == .opening {
+            return nextElement
+        }
+
+        if case ExpressionElement.comparisonOperator? = expression.first {
+            /** we have a comparison operator as the next element so
+            try to get an expression from the next two elements with the current one*/
+            let comparisonExpression = [nextElement, expression.popFirst(), expression.popFirst()].compactMap { $0 }
+            guard !comparisonExpression.isEmpty else {
+                throw ExpressionError.invalidGrammar("Only a comparison expression can follow a \(currentToken.description)")
+            }
+            let result = try evaluate(comparison: Expression(comparisonExpression)) // using .map seems to prevent to initialize the Expression as an array literal
+            return .operand(.boolean(result))
+        } else {
+            // try to get a single element expression from the next element
+            let singleExpressionResult = try evaluate(singleBooleanExpression: Expression([nextElement]))
+            return .operand(.boolean(singleExpressionResult))
+        }
+    }
+
+    /// Validate a next token when the current one is a closing bracket and returns it
+    mutating func currentTokenClosingBracketNextToken() throws -> ExpressionElement? {
+        guard let nextToken = expression.popFirst() else {
+            return nil
+        }
+
+        switch nextToken {
+        case .logicOperator, .bracket(.closing):
+            return nextToken
+        default:
+            throw ExpressionError.invalidGrammar("Chaining a closing bracket with \(nextToken.description) is invalid")
+        }
+    }
+
+    // MARK: Evaluation
+
+    /**
+     Evaluate a comparison expression like `variable = 2`
+     - note: A comparison expression should have at least one variable element as an operand. Otherwise, the expression can already be evaluated without
+     the need of a computer, since with only take care of boolean expressions
+     */
+    func evaluate(comparison expression: Expression) throws -> Bool {
+
+        guard expression.count == 3 else {
+            throw ExpressionError.invalidExpression("Trying to evaluate a comparison which has not exactly three elements")
+        }
+        guard case let ExpressionElement.comparisonOperator(comparisonOperator) = expression[1] else {
+            throw ExpressionError.invalidExpression("Trying to evaluate a comparison which doesn't have a comparison operator")
+        }
+        guard case let ExpressionElement.operand(leftOperand) = expression[0] else {
+            throw ExpressionError.invalidExpression("Trying to evaluate a comparison which doesn't have a left operand")
+        }
+        guard case let ExpressionElement.operand(rightOperand) = expression[2] else {
+            throw ExpressionError.invalidExpression("Trying to evaluate a comparison which doesn't have a right operand")
+        }
+        return try evaluateCleanComparison(leftOperand, comparisonOperator, rightOperand)
+    }
+
+    func evaluate(singleBooleanExpression expression: Expression) throws -> Bool {
+        guard expression.count == 1 else {
+            throw ExpressionError.invalidGrammar("Single boolean expression with more than two elements: \(expression.description)")
+        }
+        guard case let ExpressionElement.operand(.variable(variableName)) = expression[0] else {
+            throw ExpressionError.invalidGrammar("Trying to evaluate a single element which is not a variable: \(expression.description)")
+        }
+        guard let variableValue = variables[variableName] else {
+            throw ExpressionError.undefinedVariable(variableName)
+        }
+        guard let boolean = Bool(variableValue) else {
+            throw ExpressionError.invalidGrammar("Trying to evaluate a single variable which has not a boolean value: \(expression.description)")
+        }
+        return boolean
+    }
+
+    /**
+     Evaluate a comparison expression which has passed the check. This function should not be called directly
+     */
+    func evaluateCleanComparison(_ leftOperand: ExpressionElement.Operand,
+                                 _ comparisonOperator: Operator,
+                                 _ rightOperand: ExpressionElement.Operand) throws -> Bool {
+
+        // get the left or right variable value
+        let variableName: String
+        let isLeftOperandAVariable: Bool
+        var remainingOperand: ExpressionElement.Operand
+
+        if case let ExpressionElement.Operand.variable(name) = leftOperand {
+            variableName = name
+            isLeftOperandAVariable = true
+            remainingOperand = rightOperand
+        } else if case let ExpressionElement.Operand.variable(name) = rightOperand {
+            variableName = name
+            isLeftOperandAVariable = false
+            remainingOperand = leftOperand
+        } else {
+            let expressionDescription = "\(leftOperand.description) \(comparisonOperator.description), \(rightOperand.description)"
+            throw ExpressionError.invalidExpression("Trying to evaluate a comparison which doesn't have at least one variable for operand: \(expressionDescription)")
+        }
+
+        guard let variableValue = variables[variableName] else {
+            throw ExpressionError.undefinedVariable(variableName)
+        }
+
+        var result: Bool?
+
+        switch remainingOperand {
+
+        case .string(let string):
+            result = isLeftOperandAVariable ? comparisonOperator.evaluate(variableValue, string) : comparisonOperator.evaluate(string, variableValue)
+
+        case .number(let double):
+            guard let doubleVariableValue = Double(variableValue) else {
+                throw ExpressionError.mismatchingType
+            }
+            result = isLeftOperandAVariable ? comparisonOperator.evaluate(doubleVariableValue, double) : comparisonOperator.evaluate(double, doubleVariableValue)
+
+        case .boolean(let boolean):
+            guard let booleanVariableValue = Bool(variableValue) else {
+                throw ExpressionError.mismatchingType
+            }
+            result = isLeftOperandAVariable ? comparisonOperator.evaluate(booleanVariableValue, boolean) : comparisonOperator.evaluate(boolean, booleanVariableValue)
+
+        case .variable(let otherVariableName):
+            guard let otherVariableValue = variables[otherVariableName] else { return false }
+            result = isLeftOperandAVariable ? try evaluate(leftVariableValue: variableValue, comparisonOperator: comparisonOperator, rightVariableValue: otherVariableValue)
+            : try evaluate(leftVariableValue: otherVariableValue, comparisonOperator: comparisonOperator, rightVariableValue: variableValue)
+        }
+
+        guard let unwrappedResult = result else {
+            throw ExpressionError.wrongOperatorAndOperandsAssociation
+        }
+        return unwrappedResult
+    }
+
+    /// Helper to evaluate a comparison expression with two variables as operands
+    func evaluate(leftVariableValue: String, comparisonOperator: Operator, rightVariableValue: String) throws -> Bool {
+        var result: Bool?
+
+        if let leftDouble = Double(leftVariableValue), let rightDouble = Double(rightVariableValue) {
+            result = comparisonOperator.evaluate(leftDouble, rightDouble)
+        } else if let leftBoolean = Bool(leftVariableValue), let rightBoolean = Bool(rightVariableValue) {
+            result = comparisonOperator.evaluate(leftBoolean, rightBoolean)
+        } else {
+            // string comparison
+            result = comparisonOperator.evaluate(leftVariableValue, rightVariableValue)
+        }
+
+        if let unwrappedResult = result {
+            return unwrappedResult
+        } else {
+            throw ExpressionError.wrongOperatorAndOperandsAssociation
+        }
+    }
+}
---
+++ Sources/BooleanExpressionEvaluation/Expression/ExpressionElement.swift
@@ -0,0 +1,130 @@
+//
+//  GNU GPLv3
+//
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see https://www.gnu.org/licenses
+    for more information.
+*/
+
+import Foundation
+
+/// Represent an element in a boolean expression
+public enum ExpressionElement: Equatable, CustomStringConvertible, Codable {
+
+    // MARK: - Properties
+
+    // MARK: Cases
+
+    case comparisonOperator(Operator)
+    case logicOperator(LogicOperator)
+    case bracket(Bracket)
+    case operand(Operand)
+
+    // MARK: - Associated enums
+
+    public enum Bracket: String {
+        case opening = "("
+        case closing = ")"
+    }
+
+    public enum Operand: Equatable, CustomStringConvertible {
+        case variable(String)
+        case string(String)
+        case number(Double)
+        case boolean(Bool)
+
+        init(_ element: String, variablesRegexPattern: String? = nil) throws {
+            if element.first == "\"" && element.last == "\"" {
+                var string = element
+                string.removeFirst()
+                string.removeLast()
+                guard !string.contains("\"") else {
+                    throw ExpressionError.invalidStringQuotation(element)
+                }
+                self = .string(string)
+            } else if let double = Double(element) {
+                self = .number(double)
+            } else if let boolean = Bool(element) {
+                self = .boolean(boolean)
+            } else {
+                let pattern = variablesRegexPattern ?? Self.variableRegexPattern
+                guard let regex = try? NSRegularExpression(pattern: pattern, options: []),
+                regex.matchFoundIn(element) else {
+                    throw ExpressionError.invalidVariableName(element)
+                }
+                self = .variable(element)
+            }
+        }
+
+        public var description: String {
+            switch self {
+            case .variable(let variableName): return variableName
+            case .string(let string): return  "'\(string)'"
+            case .number(let double): return double.description
+            case .boolean(let boolean): return boolean.description
+            }
+        }
+
+        static let booleanRegexPattern = "true|false"
+        static let numberRegexPattern = #"[0-9\.]+"#
+        static let stringRegexPattern =  #"".*""#
+        static let variableRegexPattern = "[a-zA-Z]{1}[a-zA-Z0-9_-]+"
+
+        static func getRegexPattern(variablesRegexPattern: String? = nil) -> String {
+            var pattern = #"\(*("#
+            pattern += #"(\#(booleanRegexPattern))|"#
+            pattern += #"(\#(numberRegexPattern))|"#
+            pattern += #"(\#(stringRegexPattern))|"#
+            pattern += #"(\#(variablesRegexPattern ?? variableRegexPattern))"#
+            pattern += #")\)*"#
+
+            return pattern
+        }
+    }
+
+    // MARK: - Properties
+
+    public var description: String {
+        switch self {
+        case .comparisonOperator(let comparisonOperator): return comparisonOperator.description
+        case .logicOperator(let logicOperator): return logicOperator.description
+        case .bracket(let bracket): return bracket.rawValue
+        case .operand(let operand): return operand.description
+        }
+    }
+
+    // MARK: - Initialization
+
+    init(element: String, variablesRegexPattern: String? = nil) throws {
+        if let comparisonOperator = Operator.findModel(with: element) {
+            self = .comparisonOperator(comparisonOperator)
+        } else if let logicOperator = LogicOperator.findModel(with: element) {
+            self = .logicOperator(logicOperator)
+        } else if let bracket = Bracket(rawValue: element) {
+            self = .bracket(bracket)
+        } else {
+            self = .operand(try Operand(element, variablesRegexPattern: variablesRegexPattern))
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let key = try decoder.singleValueContainer().decode(String.self)
+        self = try ExpressionElement(element: key)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(description)
+    }
+}
